3bffea197c8b12f69920767c48d9123504460526
Jaswir <jaswirraghoe@gmail.com>
2024-03-16 09:13:19 +0100

    Moderate forum button empty func

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,46 @@
 <template>
-  <div class="p-4">
-    <h2 class="text-center text-xl font-semibold mb-4 p-4">Forum Messages</h2>
-    <div class="message-card p-3" v-for="(message, index) in messages" :key="index">
-      <div class="w-3/6 mx-auto border rounded-lg shadow-md p-4 pl-8">
-        {{ message }}
-      </div>
-    </div>
-  </div>
 
-  <div class="w-3/6 mx-auto">
-    <div class="mb-2">
-      <textarea
-        class="p-2 form-textarea mt-1 block w-full rounded-md border-[#A3816A] border "
-        rows="4" placeholder="Enter your comment here."></textarea>
+  <div class="grid grid-cols-3 gap-4">
+    <div class="col-span-2 pl-8">
+
+      <div class="p-4">
+        <h2 class="text-center text-2xl font-semibold mb-4 p-4">Forum Messages</h2>
+        <div class="message-card p-3" v-for="(message, index) in messages" :key="index">
+          <div class="border rounded-lg shadow-md p-4 pl-8">
+            {{ message }}
+          </div>
+        </div>
+      </div>
+
+
+      <div class="mb-2">
+        <textarea class="p-2 form-textarea mt-1 block w-full rounded-md border-[#A3816A] border " rows="4"
+          placeholder="Enter your comment here." v-model="inputContent"></textarea>
+      </div>
+      <div class="text-right">
+        <button
+          class="mt-4 bg-[#0A065D] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          @click="addMessage">
+          Submit Comment
+        </button>
+      </div>
+
+
     </div>
-    <div class="text-right">
+
+    <div class="col-span-1  flex justify-center  items-center">
+
       <button
         class="mt-4 bg-[#0A065D] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        type="button">
-        Submit Comment
+        @click="moderateForum">
+        Moderate Forum
       </button>
     </div>
+
+
   </div>
+
+
 
 
 
@@ -47,33 +66,38 @@ export default {
       "User1: How are you going to deal with the investors? Some of the people on this forum ...",
       "Alice: It's okay, but there are already exists something like this"
     ])
+    const inputContent = ref('')
 
-    async function addMessage(priority) {
-      let api_url = "https://healthomatic-psi.vercel.app"
-      console.log("API URL:", api_url)
-      api_url = 'http://127.0.0.1:8000'
+    async function addMessage() {
+      console.log("added message")
+      this.messages.push(`You: ${inputContent.value}`);
 
-      axios.get(`${api_url}/patients/${priority}`)
-        .then(response => {
-          if (priority === 'Emergency') {
-            emergency_patients.value = response.data;
-          }
-        })
-        .catch(error => {
-          console.error('Error fetching patients:', error);
-        });
+
+
+
+      // let api_url = "https://healthomatic-psi.vercel.app"
+      // console.log("API URL:", api_url)
+      // api_url = 'http://127.0.0.1:8000'
+
+      // axios.get(`${api_url}/patients/${priority}`)
+      //   .then(response => {
+      //     if (priority === 'Emergency') {
+      //       emergency_patients.value = response.data;
+      //     }
+      //   })
+      //   .catch(error => {
+      //     console.error('Error fetching patients:', error);
+      //   });
     };
 
-
-    onMounted(() => {
-      fetchPatientsByPriority("Emergency");
-    });
-
+    async function moderateForum(){
+      console.log("Shows labels and descriptions")
+    }
 
 
     return {
-      emergency_patients,
-      messages
+      emergency_patients, messages, inputContent,
+      addMessage, moderateForum
     }
   }
 }
