045e86f6cdf8f6ea981d473317ddc78cd5dfaa71
Tabinda Noor <114504243+Tabindanoor@users.noreply.github.com>
2024-03-16 23:58:00 +0500

    Update App.vue
    added styles in navbar

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,18 @@
         <router-link to="/" class="text-xl font-bold">Mod GuardAIn</router-link>
       </div>
       <div class="flex items-center justify-center flex-1 space-x-4">
-        <router-link to="/" class="text-white block hover:text-gray-200">Home</router-link>
-        <router-link to="/pricing" class="text-white block hover:text-gray-200">
+        <router-link to="/" class="text-white block hover:text-gray-200 hover:font-bold">Home</router-link>
+        <router-link to="/pricing" class="text-white block hover:text-gray-200 hover:font-bold">
           Pricing</router-link>
-        <router-link to="/about" class="text-white block hover:text-gray-200">
+        <router-link to="/about" class="text-white block hover:text-gray-200 hover:font-bold">
           About</router-link>
-        <router-link to="/contact" class="text-white block hover:text-gray-200">
+        <router-link to="/contact" class="text-white block hover:text-gray-200 hover:font-bold">
           Contact</router-link>
 
 
       </div>
 
-      <router-link to="/demo" class="text-white block hover:text-gray-200 pr-8">
+      <router-link to="/demo" class="text-white block w-20 text-center p-3 rounded-lg animate-bounce border-red-700  bg-green-700 hover:text-gray-200 pr-8">
         Demo</router-link>
     </div>
   </nav>
